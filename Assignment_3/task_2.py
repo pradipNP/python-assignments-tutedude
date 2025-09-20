5836c45665b3ea3d88f650cc2388255d7e1ba464
Pradip Kumar Kohar <980pk4456@gmail.com>
2025-09-20 15:02:14 +0530

    Add new assignment_3

---
+++ Assignment_3/task_2.py
@@ -0,0 +1,11 @@
+import math
+
+num = float(input("Enter a number: "))
+
+square = math.sqrt(num)
+logarithm = math.log(num)
+sine = math.sin(num)
+
+print("Square root: ", square)
+print("Logarithm: ", logarithm)
+print("Sine: ", sine)
